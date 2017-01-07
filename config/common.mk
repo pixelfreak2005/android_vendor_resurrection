b878c2b6be6c45b0892f4235a7310d0d51127fec
pixelfreak <pixelfreak2005@googlemail.com>
2017-01-07 19:42:18 +0100

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -52,7 +52,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=2
+    ro.build.selinux=1
 
 # Enable Google Assistant on all devices.
 PRODUCT_PROPERTY_OVERRIDES += \
